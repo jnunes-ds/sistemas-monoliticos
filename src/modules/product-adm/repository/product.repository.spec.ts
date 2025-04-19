f56f499d4d477ca205fbc61099d44719cd216cf4
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-19 20:57:56 -0300

    test: create product repository

---
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -0,0 +1,53 @@
+import {Sequelize} from "sequelize-typescript";
+import {ProductModel} from "./product.model";
+import Product from "../domain/product.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import ProductRepository from "./product.repository";
+
+describe("Product Repository Test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productProps = {
+      id: new Id("123Abc"),
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    }
+
+    const product = new Product(productProps)
+    const productRepository = new ProductRepository();
+    await productRepository.add(product);
+
+    const productDb = await ProductModel.findOne({
+      where: { id: productProps.id.id }
+    });
+
+    const {dataValues} = productDb;
+
+    expect(dataValues.id).toEqual(productProps.id.id);
+    expect(dataValues.name).toEqual(productProps.name);
+    expect(dataValues.description).toEqual(productProps.description);
+    expect(dataValues.purchasePrice).toEqual(productProps.purchasePrice);
+    expect(dataValues.stock).toEqual(productProps.stock);
+    expect(dataValues.createdAt).toBeDefined();
+    expect(dataValues.updatedAt).toBeDefined();
+
+  });
+});
