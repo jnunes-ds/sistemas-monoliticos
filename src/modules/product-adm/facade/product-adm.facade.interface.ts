b2b611cb4e653b452d63f50c2685f3e3faeac474
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-20 13:41:18 -0300

    feat: create facade contract

---
+++ src/modules/product-adm/facade/product-adm.facade.interface.ts
@@ -0,0 +1,21 @@
+export interface InputAddProductFacadeDto {
+  id?: string
+  name: string
+  description: string
+  purchasePrice: number
+  stock: number
+}
+
+export interface InputCheckStockFacadeDto {
+  productId: string
+}
+
+export interface OutputCheckStockFacadeDto {
+  productId: string;
+  stock: number;
+}
+
+export default interface IProductAdmFacade {
+  addProduct(input: InputAddProductFacadeDto): Promise<void>;
+  checkStock(input: InputCheckStockFacadeDto): Promise<OutputCheckStockFacadeDto>;
+}
