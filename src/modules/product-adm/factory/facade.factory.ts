c7907c47b21cc1962b5eb59c4707c63a787f3621
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-20 20:52:26 -0300

    feat: create check stock use case and use it on product-adm facade

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,14 +1,17 @@
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
 import ProductAdmFacade from "../facade/product-adm.facade";
+import CheckStockUsecase from "../usecase/check-stock/check-stock.usecase";
 
 export default class ProductAdmFacadeFactory {
   static create() {
     const productRepository = new ProductRepository();
     const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockUsecase(productRepository);
 
     return new ProductAdmFacade({
       addUseCase: addProductUseCase,
+      checkStockUseCase: checkStockUseCase,
     });
   }
 }
