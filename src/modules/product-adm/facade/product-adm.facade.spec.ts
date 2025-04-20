fb76808c5a5f252c754d9673c4bfc52c89daa9dc
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-20 20:52:31 -0300

    test: create check stock use case and use it on product-adm facade

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -20,6 +20,7 @@ describe("Product-ADM Facade Test", () => {
   afterEach(async () => {
     await sequelize.close();
   });
+
   it("should create a product", async () => {
     const productAdmFacade = ProductAdmFacadeFactory.create();
 
@@ -41,6 +42,26 @@ describe("Product-ADM Facade Test", () => {
     expect(product.name).toBe(input.name);
     expect(product.description).toBe(input.description);
     expect(product.purchasePrice).toBe(input.purchasePrice);
+    expect(product.stock).toBe(input.stock);
+  });
+
+  it("should check product stock", async () => {
+    const productAdmFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "123Abc",
+      name: "Product 1",
+      description: "Description 1",
+      purchasePrice: 100,
+      stock: 10,
+    }
+
+    await productAdmFacade.addProduct(input);
+
+    const product = await productAdmFacade.checkStock({productId: input.id});
+
+    expect(product).toBeDefined();
+    expect(product.productId).toBe(input.id);
     expect(product.stock).toBe(input.stock);
   });
 });
