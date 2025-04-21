fe0485716c75de7884e2b3f39d333539c401aa94
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-21 14:30:57 -0300

    test: create find all products use case

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,22 @@
+import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
+import {Promise} from "ts-toolbelt/out/Any/Promise";
+import ProductGateway from "../../gateway/product.gateway";
+import {OutputFindAllProductsDTO} from "./find-all-products.dto";
+
+export default class FindAllProductsUseCase implements UseCaseInterface {
+
+  constructor(private productRepository: ProductGateway) {}
+
+  async execute(): Promise<OutputFindAllProductsDTO> {
+    const products = await this.productRepository.findAll();
+
+    return {
+      products: products.map((product) => ({
+        id: product.id.id,
+        name: product.name,
+        description: product.description,
+        salesPrice: product.salesPrice,
+      })),
+    };
+  }
+}
