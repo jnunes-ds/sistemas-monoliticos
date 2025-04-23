5f409c2b0925337e09e9689ed8b34e7c9961d17b
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-23 00:14:43 -0300

    test: client repository

---
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -0,0 +1,43 @@
+import {Sequelize} from "sequelize-typescript";
+import {ClientModel} from "./client.model";
+import ClientRepository from "./ClientRepository";
+
+describe("Client Repository Test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+    });
+
+    sequelize.addModels([ClientModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a client", async () => {
+    const client = await ClientModel.create({
+      id: "123Abc",
+      name: "Test Client",
+      email: "testclient@mail.com",
+      address: "123 Test St",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const clientRepository = new ClientRepository();
+    const result = await clientRepository.find(client.id);
+
+    expect(result.id.id).toEqual(client.id);
+    expect(result.name).toEqual(client.name);
+    expect(result.email).toEqual(client.email);
+    expect(result.address).toEqual(client.address);
+    // expect(result.createdAt).toEqual(client.createdAt);
+    // expect(result.updatedAt).toEqual(client.updatedAt);
+  })
+});
