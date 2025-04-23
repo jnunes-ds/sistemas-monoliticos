c0071b100588bb9d8902a4abf230a2493229e66a
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-23 14:22:27 -0300

    test: create transaction repository

---
+++ src/modules/payment/repository/transaction.repository.spec.ts
@@ -0,0 +1,44 @@
+import {Sequelize} from "sequelize-typescript";
+import TransactionModel from "./transaction.model";
+import Transaction from "../domain/transaction";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import TransactionRepository from "./transaction.repository";
+
+describe("Transaction Repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: {force: true},
+    });
+
+    sequelize.addModels([TransactionModel])
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should save a transaction", async () => {
+    const transaction = new Transaction({
+      id: new Id("123Abc"),
+      amount: 100,
+      orderId: "order-123",
+    });
+    transaction.process();
+
+    const repository =  new TransactionRepository();
+    const result = await repository.save(transaction);
+
+    expect(result.id.id).toEqual(transaction.id.id);
+    expect(result.amount).toEqual(transaction.amount);
+    expect(result.orderId).toEqual(transaction.orderId);
+    expect(result.status).toEqual(transaction.status);
+    expect(result.createdAt).toEqual(transaction.createdAt);
+    expect(result.updatedAt).toEqual(transaction.updatedAt);
+  });
+});
