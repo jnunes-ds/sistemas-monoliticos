0aa62e3e8aa43f5a96d23bb21cda9af318667604
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-23 13:48:31 -0300

    test: create process peyment use case detail:
    # it should decline a payment if amount is less than 100

--- src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -6,12 +6,24 @@ const transaction: Transaction = new Transaction({
   id: new Id("123Abc"),
   orderId: "123",
   amount: 100,
+  status: "approved",
 });
 
-transaction.process();
 
 const mockRepository = () => ({
   save: jest.fn().mockResolvedValue(Promise.resolve(transaction)),
+});
+
+const transaction2: Transaction = new Transaction({
+  id: new Id("123Abc"),
+  orderId: "123",
+  amount: 50,
+  status: "declined",
+});
+
+
+const mockRepository2 = () => ({
+  save: jest.fn().mockResolvedValue(Promise.resolve(transaction2)),
 });
 
 describe("Process Payment Usecase Unit Test", () => {
@@ -32,5 +44,24 @@ describe("Process Payment Usecase Unit Test", () => {
     expect(result.orderId).toBe(transaction.orderId);
     expect(result.amount).toBe(transaction.amount);
     expect(result.status).toBe("approved");
+  });
+
+  it("should decline a payment if amount is less than 100", async () => {
+      const paymentRepository2 = mockRepository2();
+      const usecase = new ProcessPaymentUsecase(paymentRepository2);
+
+      const input = {
+        orderId: "123",
+        amount: 50,
+      };
+
+      const result = await usecase.execute(input);
+
+      expect(result).toBeDefined();
+      expect(paymentRepository2.save).toHaveBeenCalled();
+      expect(result.transactionId).toBe(transaction2.id.id);
+      expect(result.orderId).toBe(transaction2.orderId);
+      expect(result.amount).toBe(transaction2.amount);
+      expect(result.status).toBe("declined");
   });
 });
