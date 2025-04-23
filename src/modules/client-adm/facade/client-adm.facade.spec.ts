0b7c0a3063ec273eadd720f3a21c7716307d21f1
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-23 01:02:39 -0300

    test: client adm facade

--- src/modules/client-adm/facade/client-adm.facade.spec.ts
+++ src/modules/client-adm/facade/client-adm.facade.spec.ts
@@ -3,6 +3,9 @@ import ClientRepository from "../repository/ClientRepository";
 import AddClientUsecase from "../usecase/add-client/add-client.usecase";
 import {ClientModel} from "../repository/client.model";
 import ClientAdmFacade from "./client-adm.facade";
+import FindClientUsecase from "../usecase/find-client/find-client.usecase";
+import Client from "../domain/client.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
 
 describe("Client-Adm Facade Test", () => {
   let sequelize: Sequelize;
@@ -43,5 +46,28 @@ describe("Client-Adm Facade Test", () => {
     expect(client.name).toEqual(input.name);
     expect(client.email).toEqual(input.email);
     expect(client.address).toEqual(input.address);
+  });
+
+  it("should find a client", async () => {
+    const clientRepository = new ClientRepository();
+    const findClientUseCase = new FindClientUsecase(clientRepository);
+    const facade = new ClientAdmFacade({addClientUseCase: undefined, findClientUseCase});
+
+      await clientRepository.add(new Client({
+        id: new Id("123ABC"),
+        name: "Test Client Name",
+        email: "test@mail.com",
+        address: "Test Address",
+      }));
+
+    const response = await facade.find({id: "123ABC"});
+
+    expect(response).toBeDefined();
+    expect(response.id).toEqual("123ABC");
+    expect(response.name).toEqual("Test Client Name");
+    expect(response.email).toEqual("test@mail.com");
+    expect(response.address).toEqual("Test Address");
+    expect(response.createdAt).toBeDefined();
+    expect(response.updatedAt).toBeDefined();
   });
 });
