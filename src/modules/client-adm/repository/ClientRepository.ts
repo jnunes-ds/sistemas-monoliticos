fd7c85415252a00dc885ea31e0ef84d702c02220
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-23 00:20:03 -0300

    feat: passing createdAt and updatedAt properties in order to instance the object right

--- src/modules/client-adm/repository/ClientRepository.ts
+++ src/modules/client-adm/repository/ClientRepository.ts
@@ -20,6 +20,8 @@ export default class ClientRepository implements ClientGateway {
       name: client.name,
       email: client.email,
       address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
     })
   }
 }
