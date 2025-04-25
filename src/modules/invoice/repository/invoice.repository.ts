e42d4d296e1b7120181cf5d1614316003e11bfdf
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-25 14:17:41 -0300

    feat: invoice repository:
    # Method:
    ## find

--- src/modules/invoice/repository/invoice.repository.ts
+++ src/modules/invoice/repository/invoice.repository.ts
@@ -2,6 +2,9 @@ import InvoiceGateway from "../gateway/invoice.gateway";
 import Invoice from "../domain/entity/invoice.entity";
 import InvoiceModel from "./models/invoice.model";
 import InvoiceItemModel from "./models/invoice-item.model";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Address from "../domain/value-object/address.value-object";
+import InvoiceItem from "../domain/entity/invoice-item.entity";
 
 export default class InvoiceRepository implements InvoiceGateway {
   async create(input: Invoice): Promise<void> {
@@ -17,17 +20,48 @@ export default class InvoiceRepository implements InvoiceGateway {
       zipCode: input.address.zipCode,
       createdAt: new Date(),
       updatedAt: new Date(),
-      items: input.items.map((item) => ({
-        id: item.id.id,
-        name: item.name,
-        price: item.price,
-      })),
     }, {
       include: [InvoiceItemModel]
     })
+
+    await Promise.all(input.items.map((item) => {
+      return InvoiceItemModel.create({
+        id: item.id.id,
+        invoiceId: input.id.id,
+        name: item.name,
+        price: item.price,
+      })
+    }))
   }
 
-  async find(id: string): Promise<any> {
-    throw new Error("Not implemented");
+  async find(id: string): Promise<Invoice> {
+    const invoice = await InvoiceModel.findOne({where: {id}});
+
+    if (!invoice) {
+      throw new Error("Invoice not found");
+    }
+
+    const invoiceItems = await InvoiceItemModel.findAll({where: {invoiceId: invoice.id}});
+
+    return new Invoice({
+      id: new Id(invoice.id),
+      name: invoice.name,
+      document: invoice.document,
+      address: new Address({
+        street: invoice.street,
+        number: invoice.number,
+        complement: invoice.complement,
+        city: invoice.city,
+        state: invoice.state,
+        zipCode: invoice.zipCode,
+      }),
+      items: invoiceItems.map(item => ({
+        id: new Id(item.id),
+        name: item.name,
+        price: item.price,
+      })),
+      createdAt: invoice.createdAt,
+      updatedAt: invoice.updatedAt,
+    })
   }
 }
