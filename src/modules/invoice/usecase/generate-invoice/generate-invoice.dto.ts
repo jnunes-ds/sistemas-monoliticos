fe05f8a0c7d962ac8c003306f8d8204d4dab32f0
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-25 13:22:58 -0300

    feat: generate invoice use case

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.dto.ts
@@ -0,0 +1,33 @@
+export interface InputGenerateInvoiceUseCaseDTO {
+  name: string;
+  document: string;
+  street: string;
+  number: string;
+  complement: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  items: {
+    id: string;
+    name: string;
+    price: number;
+  }[];
+}
+
+export interface OutputGenerateInvoiceUseCaseDTO {
+  id: string;
+  name: string;
+  document: string;
+  street: string;
+  number: string;
+  complement: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  items: {
+    id: string;
+    name: string;
+    price: number;
+  }[];
+  total: number;
+}
