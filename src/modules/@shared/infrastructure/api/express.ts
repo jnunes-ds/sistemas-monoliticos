e1c35849f300cb4f441c78706423f78938084c67
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-30 19:37:08 -0300

    feat: product-adm api

--- src/modules/@shared/infrastructure/api/express.ts
+++ src/modules/@shared/infrastructure/api/express.ts
@@ -1,8 +1,11 @@
 import express, {Express} from "express";
 import {Sequelize} from "sequelize-typescript";
+import {productAdmRoute} from "@product-adm/infrastructure/api/product-adm.route";
+import ProductModel from "@product-adm/infrastructure/repository/sequelize/product.model";
 
 export const app: Express = express();
 app.use(express.json());
+app.use("/api/v1/products", productAdmRoute);
 
 export let sequelize: Sequelize;
 
@@ -12,6 +15,7 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   })
-
+  sequelize.addModels([ProductModel]);
+  await sequelize.sync({force: true});
 }
 setupDb();
---
+++ src/modules/product-adm/infrastructure/api/product-adm.route.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import AddProductUseCase from "@product-adm/usecase/add-product/add-product.usecase";
+import ProductRepository from "@product-adm/infrastructure/repository/sequelize/product.repository";
+import Id from "@shared/domain/value-object/id.value-object";
+
+export const productAdmRoute = express.Router();
+
+productAdmRoute.post("/", async (req, res) => {
+  const usecase = new AddProductUseCase(new ProductRepository());
+  try {
+    const productDto = {
+      id: req.body.id,
+      name: req.body.name,
+      description: req.body.description,
+      purchasePrice: req.body.purchasePrice,
+      stock: req.body.stock,
+    };
+
+    const output = await usecase.execute(productDto);
+
+  console.log("ta aqui")
+    res.status(200).json(output);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+})
