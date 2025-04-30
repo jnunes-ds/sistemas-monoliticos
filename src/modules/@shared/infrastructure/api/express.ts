af8291628cd87e574b61e0bcf96c5b737740b9f9
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-30 20:08:57 -0300

    feat: client-adm api

--- src/modules/@shared/infrastructure/api/express.ts
+++ src/modules/@shared/infrastructure/api/express.ts
@@ -2,10 +2,13 @@ import express, {Express} from "express";
 import {Sequelize} from "sequelize-typescript";
 import {productAdmRoute} from "@product-adm/infrastructure/api/product-adm.route";
 import ProductModel from "@product-adm/infrastructure/repository/sequelize/product.model";
+import {clientAdmRoute} from "@client-adm/infrastructure/api/client-adm.route";
+import ClientModel from "@client-adm/infrastructure/repository/sequelize/client.model";
 
 export const app: Express = express();
 app.use(express.json());
 app.use("/api/v1/products", productAdmRoute);
+app.use("/api/v1/clients", clientAdmRoute);
 
 export let sequelize: Sequelize;
 
@@ -15,7 +18,7 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   })
-  sequelize.addModels([ProductModel]);
+  sequelize.addModels([ProductModel,ClientModel]);
   await sequelize.sync({force: true});
 }
 setupDb();
---
+++ src/modules/client-adm/infrastructure/api/client-adm.route.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import AddClientUsecase from "@client-adm/usecase/add-client/add-client.usecase";
+import ClientRepository from "@client-adm/infrastructure/repository/sequelize/ClientRepository";
+import {InputAddClientDTO} from "@client-adm/usecase/add-client/add-client.dto";
+
+export const clientAdmRoute = express.Router();
+
+clientAdmRoute.post("/", async (req, res) => {
+  const usecase = new AddClientUsecase(new ClientRepository());
+
+  const input: InputAddClientDTO = {
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+    address: {
+      city: req.body.address.city,
+      complement: req.body.address.complement,
+      number: req.body.address.number,
+      state: req.body.address.state,
+      street: req.body.address.street,
+      zipCode: req.body.address.zipCode,
+    },
+    document: req.body.document,
+  };
+
+  try {
+    const output = await usecase.execute(input);
+    res.status(200).json(output);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: error});
+  }
+})
