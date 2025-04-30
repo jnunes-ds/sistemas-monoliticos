15aa1ff2652a49c5860e59618f34dfae43f13805
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-30 19:37:12 -0300

    test: product-adm api

---
+++ src/modules/product-adm/infrastructure/api/product-adm.e2e.spec.ts
@@ -0,0 +1,27 @@
+import {app, sequelize} from "@shared/infrastructure/api/express";
+import request from "supertest";
+
+describe("E2E Tests for Product API", () => {
+  beforeEach(async () => {
+    await sequelize.sync({force: true});
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const response = await request(app)
+      .post("/api/v1/products")
+      .send({
+        id: "123Abc",
+        name: "Test Product",
+        description: "Test description",
+        purchasePrice: 100,
+        stock: 10,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toBe("123Abc");
+  });
+});
